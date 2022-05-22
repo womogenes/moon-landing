0946ae62ced6e77dcebc48c1299123fe24447ca2
William Y. Feng <womogenes2@gmail.com>
2022-05-22 13:30:00 -0700

    Add back fancy orbit stuff

--- js/Rocket.js
+++ js/Rocket.js
@@ -41,7 +41,43 @@ export class Rocket {
     // Small timesteps
     verletStep(this.pos, this.vel, this.getAcc(), dt);
 
-    // Don't do orbit analysis :P
+    /*
+      Orbit analysis!
+      This is super haphazard, will improve later
+        - https://space.stackexchange.com/questions/1904/how-to-programmatically-calculate-orbital-elements-using-position-velocity-vecto
+        https://space.stackexchange.com/questions/8911/determining-orbital-position-at-a-future-point-in-time
+    */
+    let rVec = Vector.sub(this.pos, moon.pos);
+    let vVec = this.vel;
+
+    let v = vVec.mag();
+    let r = Vector.sub(this.pos, moon.pos).mag();
+
+    let eVec = Vector.sub(
+      Vector.mult(rVec, v * v - moon.mu / r),
+      Vector.mult(vVec, rVec.dot(vVec))
+    ).div(moon.mu);
+
+    let e = eVec.mag();
+    let E = (v * v) / 2 - moon.mu / r; // Specific mechanical energy
+    let a = -moon.mu / (2 * E); // Semi-major axis
+    let b = a * Math.sqrt(1 - e * e); // Semi-minor axis
+
+    if (e >= 1) {
+      // Weird parabolic or hyperbolic orbits; can't deal with this now
+      return;
+    }
+
+    let nu = Math.acos(eVec.dot(rVec) / (e * r));
+    if (rVec.dot(vVec) < 0) {
+      nu = Math.PI * 2 - nu;
+    }
+
+    // Find the periapsis ig
+    let apsis = Vector.sub(this.pos, moon.pos).rotate(-nu).normalize();
+    let periapsis = Vector.mult(apsis, a * (1 - e));
+    let apoapsis = Vector.mult(apsis, -a * (1 + e));
+
     // Just integrate!
     let curPos = this.pos.copy();
     let curVel = this.vel.copy();
